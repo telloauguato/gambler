9d56e97c7aa49f8067bb124ce1dd3c0affcb1263
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-16 19:48:25 -0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,11 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
       <Head>
         <title>Gambler</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
-
-      <main className=''>
-        <h1 className=''>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className=''>
-          Get started by editing
-          <code className=''>pages/index.js</code>
-        </p>
-
-        <div className=''>
-          <a href="https://nextjs.org/docs" className=''>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className=''>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className=''
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className=''
-
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className=''>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className='' />
-        </a>
-      </footer>
     </div>
-  )
+            )
 }
