121d8c84f108eb68f063002abfe47a50eeed141c
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-16 20:40:06 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Head from 'next/head'
 
 export default function Home() {
 
-  const [cota, setCota] = useState('')
-
   return (
     <>
       <Head>
@@ -38,7 +36,7 @@ export default function Home() {
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="XXX000000XXXXXX"
                   value={cota}
-                  onChange={setCota}
+                  onChange={(e) => setCota(e.target.value)}
                 />
               </div>
             </div>
