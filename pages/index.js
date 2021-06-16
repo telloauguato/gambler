063ca1b99815314a767f2bf1012612dbe3887219
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-16 19:36:43 -0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className=''>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Gambler</title>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <main className=''>
