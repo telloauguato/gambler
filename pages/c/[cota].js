6296cefe3325b8d3e9395ffe61db20a43e5ce3c3
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-18 14:05:10 -0300

    Update [cota].js

--- pages/c/[cota].js
+++ pages/c/[cota].js
@@ -86,7 +86,7 @@ export async function getServerSideProps({ params }) {
 
   return {
     props: {
-      results: filteredGame,
+      results: jsonGame,
       //bet: filteredQoute,
       params
     }
