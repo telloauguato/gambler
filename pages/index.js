d39090488952f2825c8315cdde3446fc99bb6dc6
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-18 12:23:25 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
                   type="text"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="XXX000000XXXXXX"
+                  placeholder="XXXXXXXXXXXXXXX"
                   value={cota}
                   onChange={(e) => setCota(e.target.value)}
                 />
@@ -46,12 +46,11 @@ export default function Home() {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                disabled={cota.length !== 9 ? true : false}
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <i className="ri-search-line"></i>
                 </span>
-                Buscar
+                Buscar &mdash; {cota.length}
               </button>
             </div>
           </form>
