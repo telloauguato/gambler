cda4239ab5d3b0478b5b2c66b79fe72fc5ea8141
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-18 12:25:54 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,13 +44,13 @@ export default function Home() {
             </div>
             <div>
               <button
-                type="submit"
+                type={cota.length === 15 ? "submit" : "button"}
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <i className="ri-search-line"></i>
                 </span>
-                Buscar &mdash; {cota.length}
+                Buscar
               </button>
             </div>
           </form>
