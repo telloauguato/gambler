a1e12b29d47abeb93617bdce17943091502f9598
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2021-06-20 10:50:17 -0300

    Update [cota].js

--- pages/c/[cota].js
+++ pages/c/[cota].js
@@ -31,7 +31,7 @@ export default async function Cota() {
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Gambler</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
-              {c}
+              g
             </p>
           </div>
         </div>
